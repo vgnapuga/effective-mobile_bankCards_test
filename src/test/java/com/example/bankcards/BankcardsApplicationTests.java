c3d2c39279119fc24f389a309b653e7a979b6ac0
OneNull <shavrin_ns@mail.ru>
2025-09-20 10:59:32 +0300

    Spring initialized with dependencies: 'Spring Boot Dev Tools', 'Lombok', 'Spring Security', 'Spring Data JPA', 'Liquibase Migration', 'PostgreSQL Driver', 'Docker Compose Support', 'Spring Web' && .gitignore changed

---
+++ src/test/java/com/example/bankcards/BankcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
