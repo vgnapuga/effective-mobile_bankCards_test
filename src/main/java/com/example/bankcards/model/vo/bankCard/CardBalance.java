df8f44e1bc0c5b4237f1993b05d233c7c929e306
OneNull <shavrin_ns@mail.ru>
2025-09-21 13:52:33 +0300

    CardBalance.java extends BaseValueObject.java like value object realization

---
+++ src/main/java/com/example/bankcards/model/vo/bankCard/CardBalance.java
@@ -0,0 +1,25 @@
+package com.example.bankcards.model.vo.bankCard;
+
+
+import java.math.BigDecimal;
+
+import com.example.bankcards.exception.DomainValidationException;
+import com.example.bankcards.model.vo.BaseValueObject;
+
+
+public final class CardBalance extends BaseValueObject<BigDecimal> {
+
+    public CardBalance(final BigDecimal value) {
+        super(value);
+    }
+
+    @Override
+    protected void checkValidation(final BigDecimal value) {
+        if (value.scale() > 2)
+            throw new DomainValidationException("Card balance cannot have more than 2 decimal places");
+
+        if (value.compareTo(BigDecimal.ZERO) < 0)
+            throw new DomainValidationException(String.format("Card balance < 0 (%s)", value.toString()));
+    }
+
+}
