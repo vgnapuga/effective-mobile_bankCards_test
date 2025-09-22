545ff83f59b034de94a8fa9391d3e2fc7bc634f3
OneNull <shavrin_ns@mail.ru>
2025-09-22 15:21:29 +0300

    CardEncryptionException.java added 3 nested static classes

--- src/main/java/com/example/bankcards/exception/CardEncryptionException.java
+++ src/main/java/com/example/bankcards/exception/CardEncryptionException.java
@@ -1,7 +1,7 @@
 package com.example.bankcards.exception;
 
 
-public final class CardEncryptionException extends RuntimeException {
+public class CardEncryptionException extends RuntimeException {
 
     public CardEncryptionException(final String message) {
         super(message);
@@ -9,6 +9,38 @@ public final class CardEncryptionException extends RuntimeException {
 
     public CardEncryptionException(final String message, final Throwable cause) {
         super(message, cause);
+    }
+
+    public static class InvalidKeyException extends CardEncryptionException {
+
+        public InvalidKeyException(final String message) {
+            super(message);
+        }
+
+    }
+
+    public static class EncryptionFailedException extends CardEncryptionException {
+
+        public EncryptionFailedException(final String message) {
+            super(message);
+        }
+
+        public EncryptionFailedException(final String message, final Throwable cause) {
+            super(message, cause);
+        }
+
+    }
+
+    public static class DecryptionFailedException extends CardEncryptionException {
+
+        public DecryptionFailedException(final String message) {
+            super(message);
+        }
+
+        public DecryptionFailedException(final String message, final Throwable cause) {
+            super(message, cause);
+        }
+
     }
 
 }
