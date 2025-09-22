781e8ae66a77316be35987c557b5ff65400edb91
OneNull <shavrin_ns@mail.ru>
2025-09-22 12:18:39 +0300

    CardStatus.java realization changed(vo -> enum)

--- src/main/java/com/example/bankcards/model/card/BankCard.java
+++
@@ -1,6 +0,0 @@
-package com.example.bankcards.model.card;
-
-
-public class BankCard {
-
-}
---
+++ src/main/java/com/example/bankcards/model/card/CardStatus.java
@@ -0,0 +1,8 @@
+package com.example.bankcards.model.card;
+
+
+public enum CardStatus {
+
+    ACTIVE, BLOCKED, EXPIRED
+
+}
