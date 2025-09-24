53b8436b8f7d34fe04c60d468625472591ab8aa5
OneNull <shavrin_ns@mail.ru>
2025-09-24 12:15:29 +0300

    Abstract BaseService.java realization

---
+++ src/main/java/com/example/bankcards/service/BaseService.java
@@ -0,0 +1,32 @@
+package com.example.bankcards.service;
+
+
+import com.example.bankcards.exception.BusinessRuleViolationException;
+import com.example.bankcards.exception.DomainValidationException;
+
+
+public abstract class BaseService {
+
+    private static final String NULL_ID_MESSAGE = "Entity id is <null>";
+
+    private static final String TEMPLATE_LESS_THAN_ONE_ID_MESSAGE = "Entity id must be positive (actual: %d)";
+    private static final String TEMPLATE_NEGATIVE_PAGE_MESSAGE = "Pagination page must be positive or zero (actual: %d)";
+    private static final String TEMPLATE_LESS_THAN_ONE_SIZE_MESSAGE = "Pagination size must be positive (actual: %d)";
+
+    protected final void validateId(final Long id) {
+        if (id == null)
+            throw new DomainValidationException(NULL_ID_MESSAGE);
+
+        if (id <= 0)
+            throw new BusinessRuleViolationException(String.format(TEMPLATE_LESS_THAN_ONE_ID_MESSAGE, id));
+    }
+
+    protected final void validatePagination(final int page, final int size) {
+        if (page <= 1)
+            throw new BusinessRuleViolationException(String.format(TEMPLATE_NEGATIVE_PAGE_MESSAGE, page));
+
+        if (size <= 0)
+            throw new BusinessRuleViolationException(String.format(TEMPLATE_LESS_THAN_ONE_SIZE_MESSAGE, size));
+    }
+
+}
