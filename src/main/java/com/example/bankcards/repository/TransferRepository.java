f430b76f754b7a614c00fee12e42f9882b5f6554
OneNull <shavrin_ns@mail.ru>
2025-09-24 21:37:28 +0300

    TransferService.java realization

--- src/main/java/com/example/bankcards/repository/TransferRepository.java
+++ src/main/java/com/example/bankcards/repository/TransferRepository.java
@@ -1,13 +1,22 @@
 package com.example.bankcards.repository;
 
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.bankcards.model.transfer.Transfer;
+import com.example.bankcards.model.user.User;
 
 
 @Repository
 public interface TransferRepository extends JpaRepository<Transfer, Long> {
+
+    List<Transfer> findAllByOwner(final User owner);
+
+    Page<Transfer> findAllByOwner(final User owner, Pageable pageable);
 
 }
---
+++ src/main/java/com/example/bankcards/service/TransferService.java
@@ -0,0 +1,63 @@
+package com.example.bankcards.service;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.bankcards.dto.transfer.request.TransferRequest;
+import com.example.bankcards.model.card.Card;
+import com.example.bankcards.model.transfer.Transfer;
+import com.example.bankcards.model.transfer.vo.Amount;
+import com.example.bankcards.model.user.User;
+import com.example.bankcards.repository.TransferRepository;
+import com.example.bankcards.service.card.UserCardService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TransferService extends BaseService {
+
+    private TransferRepository transferRepository;
+    private UserService userService;
+    private UserCardService userCardService;
+
+    @Transactional
+    public final Transfer transferBetweenOwnCards(final Long ownerId, final TransferRequest request) {
+        validateId(ownerId);
+
+        Long fromCardId = request.fromCardId();
+        Long toCardId = request.toCardId();
+        Amount amount = new Amount(request.amount());
+
+        validateId(fromCardId);
+        validateId(toCardId);
+
+        User owner = userService.findUserById(ownerId);
+        Card fromCard = userCardService.findCardByIdForOwner(fromCardId, owner);
+        Card toCard = userCardService.findCardByIdForOwner(toCardId, owner);
+
+        fromCard.subtractBalance(amount);
+        toCard.addBalance(amount);
+
+        Transfer transfer = Transfer.of(owner, fromCard, toCard, amount);
+
+        return transferRepository.save(transfer);
+    }
+
+    @Transactional(readOnly = true)
+    public final Page<Transfer> getAllTransfersForUser(final Long ownerId, final Pageable pageable) {
+        validatePagination(pageable);
+
+        validateId(ownerId);
+        User owner = userService.findUserById(ownerId);
+
+        return transferRepository.findAllByOwner(owner, pageable);
+    }
+
+}
