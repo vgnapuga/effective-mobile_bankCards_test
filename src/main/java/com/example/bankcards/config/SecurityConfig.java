853b13d65531059e54fe8aae079aa0cf1035e590
OneNull <shavrin_ns@mail.ru>
2025-09-25 13:31:20 +0300

    SecurityConfig.java updated for JWT auth

--- src/main/java/com/example/bankcards/config/SecurityConfig.java
+++ src/main/java/com/example/bankcards/config/SecurityConfig.java
@@ -4,18 +4,45 @@ package com.example.bankcards.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.example.bankcards.security.CardEncryption;
+import com.example.bankcards.security.jwt.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public final class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
 
     @Bean
-    public CardEncryption cardEncryption(@Value("${app.security.card-encryption-key}") final String secretKey) {
-        return CardEncryption.of(secretKey);
+    public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
+                auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
     @Bean
@@ -23,4 +50,8 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public CardEncryption cardEncryption(@Value("${app.security.card-encryption-key}") final String secretKey) {
+        return CardEncryption.of(secretKey);
+    }
 }
