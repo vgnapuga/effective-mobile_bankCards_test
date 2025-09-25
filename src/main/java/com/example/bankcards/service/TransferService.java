9d94e37a71235c39748f79e1cc9fa303263d8f40
OneNull <shavrin_ns@mail.ru>
2025-09-25 19:14:47 +0300

    TransferService.java new getters realization

--- src/main/java/com/example/bankcards/service/TransferService.java
+++ src/main/java/com/example/bankcards/service/TransferService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.bankcards.dto.transfer.request.TransferRequest;
+import com.example.bankcards.exception.AccessDeniedException;
+import com.example.bankcards.exception.ResourceNotFoundException;
 import com.example.bankcards.model.card.Card;
 import com.example.bankcards.model.transfer.Transfer;
 import com.example.bankcards.model.transfer.vo.Amount;
@@ -25,6 +27,25 @@ public class TransferService extends BaseService {
     private TransferRepository transferRepository;
     private UserService userService;
     private CardService cardService;
+
+    // ---------- Helper methods ---------- //
+
+    private Transfer findTransferByIdForAdmin(final Long transferId) {
+        return transferRepository.findById(transferId).orElseThrow(
+                () -> new ResourceNotFoundException(String.format("Transfer with id=%d not found", transferId)));
+    }
+
+    private final Transfer findCardByIdForOwner(final Long transferId, final User owner) {
+        Transfer transfer = transferRepository.findById(transferId).orElseThrow(
+                () -> new ResourceNotFoundException(String.format("Card with id=%d was not found", transferId)));
+
+        if (!transfer.getOwner().equals(owner))
+            throw new AccessDeniedException(String.format("Permission to access card denied for id=%d", owner.getId()));
+
+        return transfer;
+    }
+
+    // ------------------------------------ //
 
     @Transactional
     public final Transfer transferBetweenOwnCards(final Long ownerId, final TransferRequest request) {
@@ -50,7 +71,35 @@ public class TransferService extends BaseService {
     }
 
     @Transactional(readOnly = true)
-    public final Page<Transfer> getAllTransfersForUser(final Long ownerId, final Pageable pageable) {
+    public final Transfer getTransferByIdForAdmin(final Long adminId, final Long transferId) {
+        validateId(adminId);
+        validateId(transferId);
+
+        userService.checkAdminPermissionTo("get card", adminId);
+        return findTransferByIdForAdmin(transferId);
+    }
+
+    @Transactional(readOnly = true)
+    public final Page<Transfer> getAllTransfersForAdmin(final Long adminId, final Pageable pageable) {
+        validateId(adminId);
+        userService.checkAdminPermissionTo("get all cards", adminId);
+
+        return transferRepository.findAll(pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public final Transfer getTransferByIdForOwner(final Long ownerId, final Long transferId) {
+        validateId(ownerId);
+        validateId(transferId);
+
+        User owner = userService.findUserById(ownerId);
+        Transfer transfer = findCardByIdForOwner(transferId, owner);
+
+        return transfer;
+    }
+
+    @Transactional(readOnly = true)
+    public final Page<Transfer> getAllTransfersForOwner(final Long ownerId, final Pageable pageable) {
         validatePagination(pageable);
 
         validateId(ownerId);
